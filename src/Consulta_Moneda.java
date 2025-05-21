64eeb2651bfbef7189ed06731503e970a107e843
2885nico <104214750+2885nico@users.noreply.github.com>
2025-05-21 19:29:56 -0400

    Create Consulta_Moneda.java

---
+++ src/Consulta_Moneda.java
@@ -0,0 +1,25 @@
+public class Consulta_Moneda {
+
+    Moneda buscarMoneda(string moneda,
+                        string monedaConversion,
+                        double cantidad){
+
+        URI direccion = URI.create("GET https://v6.exchangerate-api.com/v6/d92a84a0a9992205170ada26/pair/"+moneda+"/"+monedaConversion+"/"+cantidad);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        HttpResponse<String> response = null;
+        try{
+            response=client
+                    .send(request,HttpResponse.BodyHandlers.ofString());
+        } catch(IOException | InterruptedException e){
+            throw new RuntimeException(e);
+        }
+
+
+        return new Gson().fromJson(response.body(), Moneda.class);
+    }
+}
